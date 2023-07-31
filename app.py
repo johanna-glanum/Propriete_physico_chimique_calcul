7fe3b813f6e0738021fc513cb8e24276a58bfb8b
johanna-glanum <130157067+johanna-glanum@users.noreply.github.com>
2023-07-31 16:08:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 
 import streamlit as st
 
-import matplotlib.pyplot as plt
 from collections import Counter
 
 def plot_amino_acid_proportions(sequence):
@@ -11,13 +10,6 @@ def plot_amino_acid_proportions(sequence):
     amino_acids = list(amino_acid_counts.keys())
     proportions = [count / total_amino_acids * 100 for count in amino_acid_counts.values()]
 
-    # Création du graphique "plot bar"
-    plt.bar(amino_acids, proportions)
-    plt.xlabel('Acides aminés')
-    plt.ylabel('Pourcentage')
-    plt.title('Proportion des acides aminés dans la séquence peptidique')
-    plt.grid(False)
-    plt.show()
 
 sequence = st.text_input('Write a sequence')
 st.text(plot_amino_acid_proportions(sequence))
