e64e2a4da9a673c7a7e608c413ac2fed2409ad51
johanna-glanum <130157067+johanna-glanum@users.noreply.github.com>
2023-07-31 16:34:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 
 import streamlit as st
-
+from streamlit_echarts import st_echarts
 from collections import Counter
 
 def plot_amino_acid_proportions(sequence):
@@ -9,11 +9,28 @@ def plot_amino_acid_proportions(sequence):
     total_amino_acids = len(sequence)
     amino_acids = list(amino_acid_counts.keys())
     proportions = [count / total_amino_acids * 100 for count in amino_acid_counts.values()]
-    return proportions
+    return proportions, amino_acids
 
 
 sequence = st.text_input('Write a sequence')
+proportions, amino_acids = plot_amino_acid_proportions(sequence)
+
 if st.button('Calculer la proportion des AA'):
-    st.text(plot_amino_acid_proportions(sequence))
+    st.text(proportions)
 else:
     st.text('En attente d\'une séquence')
+
+
+proportions = plot_amino_acid_proportions(sequence)
+
+options = {
+    "xAxis": {
+        "type": "category",
+        "data": amino_acids,
+    },
+    "yAxis": {"type": "value"},
+    "series": [
+        {"data": proportions}
+    ],
+}
+st_echarts(options=options)
