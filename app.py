15d4adfe63ccbd467cdb101d0075f584481cdd08
johanna-glanum <130157067+johanna-glanum@users.noreply.github.com>
2023-08-01 01:16:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -17,62 +17,61 @@ def plot_amino_acid_proportions(sequence):
     return proportions, amino_acids
 
 
-while True:
-    sequence = st.text_input('Write a sequence')
-    pH = st.text_input("Donner un pH")
-    
-    if st.button("Calculer les propriétées de la séquence"):
-    
-        pHi_seq, zwiterion, valeur_pka, valeur_pkb, forme_sequence, charge_pH, soluble, borne_inf_soluble_pH, borne_sup_soluble_pH, borne_inf_pH, borne_sup_pH, pM = calcul_all(seq = sequence, pH=float(pH))
-    
-    
-        st.text("A pH = {}, la charge global est {}".format(pH, charge_pH))
-    
-        st.text("La forme de la séquence est {}".format(forme_sequence))
-    
-        st.text(f"Le phi de la sequence {sequence} est de :{pHi_seq}")
-        st.text(" ")
-        st.text(
-            f"La forme des groupe ionisé du zwiterion de la séquence {sequence} est :\n{zwiterion}")
-        st.text(" ")
-        st.text(
-            f"Les deux pka qui encadre la forme neutre de la sequence {sequence} est : {valeur_pka} et {valeur_pkb}")
-        if not soluble:
-            st.text("la mollécule n'est pas soluble à pH = {}".format(pH))
-        else:
-            st.text("la mollécule est soluble à pH = {}".format(pH))
-    
-        st.text("La sequence est soluble pour un pH dans l'intervalle [{}, {}] et [{}, {}]".format(
-            borne_inf_pH, borne_inf_soluble_pH, borne_sup_soluble_pH, borne_sup_pH))
-        st.text("Pas soluble dans l'intervalle [{}, {}]".format(
-            borne_inf_soluble_pH, borne_sup_soluble_pH))
-    
-        proportions, amino_acids = plot_amino_acid_proportions(sequence)
-    
-        st.text(amino_acids)
-        st.text(proportions)
-    
-        fig, ax = plt.subplots()
-        ax.bar(amino_acids, proportions)
-        plt.xlabel('Acides aminés')
-        plt.ylabel('Pourcentage')
-        plt.title('Proportion des acides aminés dans la séquence peptidique')
-        plt.grid(False)
-        plt.show()
-    
+sequence = st.text_input('Write a sequence')
+pH = st.text_input("Donner un pH")
+
+if st.button("Calculer les propriétées de la séquence"):
+
+    pHi_seq, zwiterion, valeur_pka, valeur_pkb, forme_sequence, charge_pH, soluble, borne_inf_soluble_pH, borne_sup_soluble_pH, borne_inf_pH, borne_sup_pH, pM = calcul_all(seq = sequence, pH=float(pH))
+
+
+    st.text("A pH = {}, la charge global est {}".format(pH, charge_pH))
+
+    st.text("La forme de la séquence est {}".format(forme_sequence))
+
+    st.text(f"Le phi de la sequence {sequence} est de :{pHi_seq}")
+    st.text(" ")
+    st.text(
+        f"La forme des groupe ionisé du zwiterion de la séquence {sequence} est :\n{zwiterion}")
+    st.text(" ")
+    st.text(
+        f"Les deux pka qui encadre la forme neutre de la sequence {sequence} est : {valeur_pka} et {valeur_pkb}")
+    if not soluble:
+        st.text("la mollécule n'est pas soluble à pH = {}".format(pH))
     else:
-        st.text('En attente d\'un pH')
-    
-    
-    if st.button("Prédire par machine learning la solubilité à pH 7 et son pI"):
-    
-        prediction_solub = predict_solubility(sequence)
-        prediction_pI = predict_pI(sequence)
-    
-        st.text("Prediction de la solubilité : ")
-        st.dataframe(prediction_solub)
-        st.text("Prédiction du pI : ")
-        st.dataframe(prediction_pI)
+        st.text("la mollécule est soluble à pH = {}".format(pH))
+
+    st.text("La sequence est soluble pour un pH dans l'intervalle [{}, {}] et [{}, {}]".format(
+        borne_inf_pH, borne_inf_soluble_pH, borne_sup_soluble_pH, borne_sup_pH))
+    st.text("Pas soluble dans l'intervalle [{}, {}]".format(
+        borne_inf_soluble_pH, borne_sup_soluble_pH))
+
+    proportions, amino_acids = plot_amino_acid_proportions(sequence)
+
+    st.text(amino_acids)
+    st.text(proportions)
+
+    fig, ax = plt.subplots()
+    ax.bar(amino_acids, proportions)
+    plt.xlabel('Acides aminés')
+    plt.ylabel('Pourcentage')
+    plt.title('Proportion des acides aminés dans la séquence peptidique')
+    plt.grid(False)
+    plt.show()
+
+else:
+    st.text('En attente d\'un pH')
+
+
+if st.button("Prédire par machine learning la solubilité à pH 7 et son pI"):
+
+    prediction_solub = predict_solubility(sequence)
+    prediction_pI = predict_pI(sequence)
+
+    st.text("Prediction de la solubilité : ")
+    st.dataframe(prediction_solub)
+    st.text("Prédiction du pI : ")
+    st.dataframe(prediction_pI)
     
 
 
